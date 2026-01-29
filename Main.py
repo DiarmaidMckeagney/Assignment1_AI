cc222cd84a9ad75c5b84de5b89df4f68f9002ac7
Peter Fitzgerald <143887269+Atychi762@users.noreply.github.com>
2026-01-29 12:49:44 +0000

    Initalizing random population (#5)

--- Main.py
+++ Main.py
@@ -1,6 +1,8 @@
 import Setup
+import Fitness
 
 if __name__ == '__main__':
     print("hello world")
     numExams, numSlots, numStudents, enrollment = Setup.read_instance("input.txt")
 
+    population = Setup.intialize_population(2, numExams, numSlots)
