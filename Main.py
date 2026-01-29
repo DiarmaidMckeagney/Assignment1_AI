5bbc905748abe76de722e5497b1c38773ae53394
Peter Fitzgerald <143887269+Atychi762@users.noreply.github.com>
2026-01-29 12:14:25 +0000

    Parsing input file (#2)
    * Adding input file parsing
    
    * Adding input.txt
    
    * removing print statements from added code

--- Main.py
+++ Main.py
@@ -1,3 +1,6 @@
+import Setup
 
 if __name__ == '__main__':
     print("hello world")
+    numExams, numSlots, numStudents, enrollment = Setup.read_instance("input.txt")
+
--- Setup.py
+++ Setup.py
@@ -1,4 +1,20 @@
+def read_instance(fileName):
+    print("Reading input file")
+    
+    file = open(fileName, 'r')
+    enrollment = file.readlines()
+    file.close()
+
+    numExams = int(enrollment[0][0])
+    numSlots = int(enrollment[0][2])
+    numStudents = int(enrollment[0][4])
+    enrollment.pop(0)
+
+    for i in range(len(enrollment)):
+        enrollment[i] = list(map(int, enrollment[i].strip().split()))
+
+    return numExams, numSlots, numStudents, enrollment
 
 
-def setup_population():
+def intialize_population():
     print("Setting up population")
