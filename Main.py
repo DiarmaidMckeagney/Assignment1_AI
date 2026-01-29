98a2d6f586fb5a971065d3a5ea297f400ec5d9ae
Peter Fitzgerald <143887269+Atychi762@users.noreply.github.com>
2026-01-29 12:07:58 +0000

    Adding input file parsing

--- Main.py
+++ Main.py
@@ -1,3 +1,10 @@
+import Setup
 
 if __name__ == '__main__':
     print("hello world")
+    numExams, numSlots, numStudents, enrollment = Setup.read_instance("input.txt")
+
+    print("Number of Exams:", numExams)
+    print("Number of Slots:", numSlots)
+    print("Number of Students:", numStudents)
+    print("Enrollment Data:", enrollment)
