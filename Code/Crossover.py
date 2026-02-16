d9ddd5d270e43110f7ebac0fcdc6574abcfc7cd0
DiarmaidMckeagney <117989065+DiarmaidMckeagney@users.noreply.github.com>
2026-02-16 13:20:37 +0000

    adding a bunch of comments and removing unnecessary code

--- Code/Crossover.py
+++ Code/Crossover.py
@@ -2,10 +2,15 @@ import random
 
 
 def crossover(parent1, parent2,slotCount):
+    #get number of times to crossover
     numIter = random.randint(1,2)
     child1, child2 = [],[]
+
     for i in range(numIter):
+        #get random point to crossover at
         splitIndex = random.randint(1,slotCount -1)
+
+        #crossover
         child1 = parent1[:splitIndex] + parent2[splitIndex:]
         child2 = parent2[:splitIndex] + parent1[splitIndex:]
 
--- Code/Tournament.py
+++ Code/Tournament.py
@@ -1,28 +1,29 @@
 import random
-
 import numpy as np
 
 TOURNAMENT_SIZE = 2
-def run_tournament(population, fitnesses):
-    randomIndxs = []
 
-    for j in range(TOURNAMENT_SIZE):
+def run_tournament(population, fitnesses):
+    randomIndxs = [] # holds tournament participants
+
+    for j in range(TOURNAMENT_SIZE): # get participants
         randomIndxs.append(np.random.randint(0, len(population) - 1))
 
-    fitnessForParticipants = []
+    fitnessForParticipants = [] # get participants fitnesses
 
     for j in range(TOURNAMENT_SIZE):
         fitnessForParticipants.append(fitnesses[randomIndxs[j]])
 
+    # get winner of tournament
     winner = evaluate_round(fitnessForParticipants)
 
-    return population[randomIndxs[winner]]
+    return population[randomIndxs[winner]] #return winner
 
 
 def evaluate_round(participants):
-    chanceFactor = random.random()
+    chanceFactor = random.random() # this adds fuzziness to the tournament
 
     if chanceFactor < 0.6:
-        return np.argmax(participants)
+        return np.argmax(participants) # there is a 60% chance that the best participant will win
     else:
-        return random.randint(0,TOURNAMENT_SIZE-1)
+        return random.randint(0,TOURNAMENT_SIZE-1) # otherwise return a random participant as the winner
